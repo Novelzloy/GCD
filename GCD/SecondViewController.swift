62449c362401212eb0aa520fd90367fb334263d6
Roman Naumov <novel_90@mail.ru>
2021-08-01 15:55:15 +0300

    was add asyncAfter

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -28,6 +28,36 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchImage()
+        delay(3) {
+            self.loginAlert()
+        }
+        
+    }
+    
+    fileprivate func delay (_ delay: Int, clouser: @escaping () -> ()){
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            clouser( )
+        }
+    }
+    
+    fileprivate func loginAlert() {
+        let alert = UIAlertController(title: "Зарегистрованны?", message: " Введите ваш логин и пароль ", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        let canceAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(okAction)
+        alert.addAction(canceAction)
+        
+        alert.addTextField { (usernameTF) in
+            usernameTF.placeholder = "Введите логин"
+        }
+        
+        alert.addTextField { (userpassTF) in
+            userpassTF.placeholder = "Введите пароль"
+            userpassTF.isSecureTextEntry = true
+        }
+        
+        self.present(alert, animated: true, completion: nil)
     }
     
     
